b88e731bfa3b49ed3d01a3cfced60a38ed0905d2
remii11 <91636388+remii11@users.noreply.github.com>
2021-12-06 12:30:24 -0800

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -142,10 +142,10 @@ def remove_special_chars(title):
 keyword_df["word_list"]=keyword_df["word_list"].apply(remove_special_chars)
 
 #makes a list of all words used, and removes 'nonkeywords'
-all_words_array= list(keyword_df["word_list"])
-all_words_list = all_words_array[0]+all_words_array[1]
-for i in range(2,len(all_words_array)):
-    all_words_list = all_words_list +all_words_array[i]
+A= list(keyword_df["word_list"])
+all_words_list = A[0]+A[1]
+for i in range(2,len(A)):
+    all_words_list = all_words_list +A[i]
 nonkeywords = ["AND","TO","YOUR","YOU","FOR","WILL","THAT"]
 for i in range(len(all_words_list)):
     for j in range(len(nonkeywords)):
